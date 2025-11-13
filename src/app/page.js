b4322b27e2a1f71c0991e9b07fa8febe7c94dd30
Sparsh Verencar <verencarsparsh@gmail.com>
2025-11-13 22:36:05 +0530

    add dark mode and freelancer dashboard

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/mode-toggle";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <Button>
-        vpop
+        sparsh
       </Button>
+        <ModeToggle/>
     </div>
   );
 }
