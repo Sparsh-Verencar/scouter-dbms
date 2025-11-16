2bd645e6b591fb9c89e0fe8778b9cdc2f92c22b9
Sparsh Verencar <verencarsparsh@gmail.com>
2025-11-16 22:40:04 +0530

    add logo to freelancer dashboard home

--- src/app/freelancer-dashboard/page.jsx
+++ src/app/freelancer-dashboard/page.jsx
@@ -1,9 +1,21 @@
 "use client"
 
+import Image from "next/image";
+
 export default function FreelancerDashboard() {
   return (
-    <h1>
-      start jobbing
-    </h1>
+   <div className="w-full h-full flex flex-col items-center justify-center">
+  <Image
+    src="/scouterLogo-removebg-preview.png"
+    alt="scouter logo"
+    width={200}   // original size
+    height={200}  // original size
+    className="w-1/3 max-w-xs h-auto" // responsive width, auto height
+  />
+  <h1 className="text-4xl dark:text-gray-600 text-gray-400 pt-10">
+    START APPLYING
+  </h1>
+</div>
+
   );
 }
